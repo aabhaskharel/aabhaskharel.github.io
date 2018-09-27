4b578c706134ef151535c027a13ec36b825d0da8
Aabhas Kharel <aabhas.kharel@mavs.uta.edu>
2018-09-26 19:23:52 -0500

    added resume and minor changes

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var main = new Vue({
     el: '#main',
     data: {
+        displayMid: true,
         input: '',
         output: '',
-        hello: [
+        greetings: [
             'hi', 'hello', 'hey'
         ],
         resume: [
-            'resume', 'cv'
+            'resume', 'cv', 'about', 'aabhas', 'kharel'
+        ],
+        education:[
+            'school','uni','college','edu','graduate','study','studied','junior','texas','arlington','uta'
         ],
         showResume: false
     },
@@ -18,17 +22,38 @@ var main = new Vue({
             if(this.input == '')
                 this.output = ''
             if (this.input == 'lol')
-                this.output = 'hahaha!'
-            for (var i in this.hello) {
-                if (this.input.toLowerCase().indexOf(this.hello[i]) > -1)
+                this.output = 'Hmm..I wonder what is so funny.'
+            
+            //greetings response
+            for (var i in this.greetings) {
+                if (this.input.toLowerCase().indexOf(this.greetings[i]) > -1)
                     return this.output = 'Hey There!'
             }
+
+            //resume response
             for (var i in this.resume) {
                 if (this.input.toLowerCase().indexOf(this.resume[i]) > -1) {
                     this.showResume = true
                     return this.output = 'Here is my '
                 }
             }
+
+            //education response
+            for (var i in this.education) {
+                if (this.input.toLowerCase().indexOf(this.education[i]) > -1) {
+                    return this.output = 'I am currently a junior majoring in Software Engineering at The University of Texas ar Arlington.'
+                }
+            }
+
+            //
+        }
+    },
+    methods:{
+        searchBoxClicked: function(){
+            this.displayMid = false;
+        },
+        searchBoxLeave: function(){
+            this.displayMid = true;
         }
     }
 });
